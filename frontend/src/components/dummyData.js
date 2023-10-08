e92d0ed7b42518466cd6c139952ea45ea992b100
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2023-10-08 14:23:04 +0530

    slot

---
+++ frontend/src/components/dummyData.js
@@ -0,0 +1,251 @@
+const slotData=[
+    {
+        floor: 0,
+        slot: 100,
+        vehicle:'',
+        name:'',
+        time:''
+
+    },
+    {
+        floor: 0,
+        slot: 101,
+        vehicle:'',
+        name:'',
+        time:''
+
+    },
+    {
+        floor: 0,
+        slot: 102,
+        vehicle:'',
+        name:'',
+        time:''
+
+    },
+    {
+        floor: 0,
+        slot: 103,
+        vehicle:'',
+        name:'',
+        time:''
+
+    },
+    {
+        floor: 0,
+        slot: 104,
+        vehicle:'',
+        name:'',
+        time:''
+
+    },
+    {
+        floor: 0,
+        slot: 105,
+        vehicle:'',
+        name:'',
+        time:''
+
+    },
+    {
+        floor: 0,
+        slot: 106,
+        vehicle:'',
+        name:'',
+        time:''
+
+    },
+    {
+        floor: 0,
+        slot: 107,
+        vehicle:'',
+        name:'',
+        time:''
+
+    },
+    {
+        floor: 0,
+        slot: 108,
+        vehicle:'',
+        name:'',
+        time:''
+
+    },
+    {
+        floor: 0,
+        slot: 109,
+        vehicle:'',
+        name:'',
+        time:''
+
+    },
+    {
+        floor: 0,
+        slot: 110,
+        vehicle:'',
+        name:'',
+        time:''
+
+    },
+    {
+        floor: 1,
+        slot: 111,
+        vehicle:'',
+        name:'',
+        time:''
+
+    },
+    {
+        floor: 1,
+        slot: 112,
+        vehicle:'',
+        name:'',
+        time:''
+
+    },
+    {
+        floor: 1,
+        slot: 113,
+        vehicle:'',
+        name:'',
+        time:''
+
+    },
+    {
+        floor: 1,
+        slot: 114,
+        vehicle:'',
+        name:'',
+        time:''
+
+    },
+    {
+        floor: 1,
+        slot: 115,
+        vehicle:'',
+        name:'',
+        time:''
+
+    },
+    {
+        floor: 1,
+        slot: 116,
+        vehicle:'',
+        name:'',
+        time:''
+
+    },
+    {
+        floor: 1,
+        slot: 117,
+        vehicle:'',
+        name:'',
+        time:''
+
+    },
+    {
+        floor: 1,
+        slot: 118,
+        vehicle:'',
+        name:'',
+        time:''
+
+    },
+    {
+        floor: 1,
+        slot: 119,
+        vehicle:'',
+        name:'',
+        time:''
+
+    },
+    {
+        floor: 1,
+        slot: 120,
+        vehicle:'',
+        name:'',
+        time:''
+
+    },
+    {
+        floor: 2,
+        slot: 121,
+        vehicle:'',
+        name:'',
+        time:''
+
+    },
+    {
+        floor: 2,
+        slot: 122,
+        vehicle:'',
+        name:'',
+        time:''
+
+    },
+    {
+        floor: 2,
+        slot: 123,
+        vehicle:'',
+        name:'',
+        time:''
+
+    },
+    {
+        floor: 2,
+        slot: 124,
+        vehicle:'',
+        name:'',
+        time:''
+
+    },
+    {
+        floor: 2,
+        slot: 125,
+        vehicle:'',
+        name:'',
+        time:''
+
+    },
+    {
+        floor: 2,
+        slot: 126,
+        vehicle:'',
+        name:'',
+        time:''
+
+    },
+    {
+        floor: 2,
+        slot: 127,
+        vehicle:'',
+        name:'',
+        time:''
+
+    },
+    {
+        floor: 2,
+        slot: 128,
+        vehicle:'',
+        name:'',
+        time:''
+
+    },
+    {
+        floor: 2,
+        slot: 129,
+        vehicle:'',
+        name:'',
+        time:''
+
+    },
+    {
+        floor: 2,
+        slot: 130,
+        vehicle:'',
+        name:'',
+        time:''
+
+    }
+];
+export default slotData;
