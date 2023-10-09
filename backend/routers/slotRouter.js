dc4aa369128322c05d49cc36e5c8cacdf9f2c9a5
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2023-10-10 01:07:42 +0530

    getall

--- backend/routers/slotRouter.js
+++ backend/routers/slotRouter.js
@@ -2,3 +2,49 @@ const express=require ('express');
 const Model = require('../models/slotModel');
 
 const router = express.Router();
+
+router.post('/add', (req, res) => {
+    console.log(req.body);
+
+    new Model(req.body).save()
+    .then((result) => {
+        res.json(result);
+    })
+    .catch((err) => {
+        console.log(err);
+        
+        res.json(err);
+    });
+});
+
+router.get('/getall', (req, res) => {
+    Model.find({})
+    .then((result) => {
+        res.json(result);
+    }).catch((err) => {
+        res.json(err);
+    });
+});
+
+router.get('/getbyslot/:slot', (req,res)=>{   
+    console.log(req.params.slot);
+    module.findOne({slot: req.params.slot})
+    .then((result)=>{
+        res.json(result);
+    }) .catch((err)=>{
+        res.json(err);
+    });
+    
+})
+
+router.get('/getbyid/:id', (req, res) => {
+    Model.findById(req.params.id)
+    .then((result)=>{            
+        res.json(result);
+    })
+    
+    .catch((err)=>{
+        res.json(err);
+    })
+    
+});
