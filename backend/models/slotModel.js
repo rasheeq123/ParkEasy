fe5607f4ccdf5ec63692dedc2fc0f962d8962373
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2023-10-10 23:38:56 +0530

    model

--- backend/models/slotModel.js
+++ backend/models/slotModel.js
@@ -3,9 +3,9 @@ const { Schema, model}= require('..connection');
 const slotschema = new Schema({
     floor: Number ,
     slot: Number,
-    vehicle: String,
-    name: String,
-    time: Date
+    vehicle_no : String,
+    time: Date,
+    user_id: Number
 });
 
 module.exports=model('parkings', slotschema);
