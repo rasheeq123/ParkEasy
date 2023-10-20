8cddddb96a730118acfcca0df66e8d358da039b1
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2023-10-21 00:55:59 +0530

    manage slot

--- frontend/src/components/ManageSlots.jsx
+++ frontend/src/components/ManageSlots.jsx
@@ -1,13 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const ManageSlots = () => {
 
-  const fetchSlots = () => {
-    
+  const [userList, setuserList] = useState([])
+
+  const fetchSlots = async ()=>{
+    const res=await fetch('http://localhost:5000/user/getall')
+    console.log(res.status);
+    const data= await res.json(); // data fetch krne ke lie
+    console.table(data);
+    setuserList(data);
   }
+    useEffect(() => {
+    // when component opens
+  
+    fetchSlots();
+    
+  }, [])
+  
 
   return (
-    <div>Manage Slots</div>
+    <div className="vh-100 bg-body-secondary">
+        <div className="container py-4">
+            <h1 className="text-center"> Manage Slot </h1>
+            <table className='table table-dark'>
+                <thead>
+                    <tr>
+                        
+                        <th>ID</th>
+                        <th>Floor</th>
+                        <th>Slot</th>
+                        <th>Vehicle No.</th>
+                        <th>Time</th>
+                    </tr>
+                </thead>
+                <tbody>
+                  {
+                    userList.map((parkings, index)=>(
+                      <tr>
+                        <td>{index+1}</td>
+                        <td>{parkings._id}</td>
+                        <td>{parkings.floor}</td>
+                        <td>{parkings.slot}</td>
+                        <td>{parkings.vehicle}</td>
+                        <td>{parkings.time}</td>
+                      </tr>
+                    ))
+                  }
+                </tbody>
+                </table>
+                </div>
+                </div>
   )
 }
 
