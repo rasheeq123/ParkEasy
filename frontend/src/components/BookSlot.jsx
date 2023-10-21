07c8e3ea789766396b0e60f65e31954ade8a6fb8
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2023-10-21 23:20:34 +0530

    yup

--- frontend/src/components/BookSlot.jsx
+++ frontend/src/components/BookSlot.jsx
@@ -1,56 +1,90 @@
 import { useFormik } from 'formik'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Swal from 'sweetalert2';
 import useAppContext from '../AppContext';
+import slotData from './dummyData';
+import * as Yup from 'yup';
+
+
+const BookSchema = Yup.object().shape({
+   floor: Yup.string()
+      
+  .required('Required'),
+   slot: Yup.string()
+      
+  .required('Required'),
+   vehicle: Yup.string()
+      
+  .required('Required')
+  
+
+          
+   });
 
 const BookSlot = () => {
 
-    const{ setloggedIn}= useAppContext();
+    const [slotsAvailable, setSlotsAvailable] = useState(slotData.map(slotData => slotData.slot));
+
+    const fetchbookedSlots = async () => {
+        const res = await fetch('http://localhost:5000/parkings/getall')
+        console.log(res.status);
+        const data = await res.json();
+        const bookedslots = data.map(slotData => slotData.slot);
+        setSlotsAvailable(slotsAvailable.filter(slotNum => !bookedslots.includes(slotNum)))
+    }
+
+    useEffect(() => {
+        fetchbookedSlots();
+    }, [])
+
+
+    const { setloggedIn } = useAppContext();
     const [currentUser, setCurrentUser] = useState(JSON.parse(sessionStorage.getItem('user')))
 
-    const bookslot = useFormik ({
-        initialValues:{
+    const bookslot = useFormik({
+        initialValues: {
             floor: 1,
-            slot:0,
-            vehicle:'',
+            slot: 0,
+            vehicle: '',
             user: currentUser._id,
-            time:new Date()
+            time: new Date()
         },
-        onSubmit: async (values)=>{
+        onSubmit: async (values) => {
             console.log(values);
-            const res = await fetch('http://localhost:5000/parkings/add',{
-            method: 'POST',
-            body:JSON.stringify(values),
-            headers:{
-                'Content-Type':'application/json'
+            const res = await fetch('http://localhost:5000/parkings/add', {
+                method: 'POST',
+                body: JSON.stringify(values),
+                headers: {
+                    'Content-Type': 'application/json'
+
+                }
+            });
+            console.log(res.status)
+            if (res.status === 200) {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Booked Successfully'
+
+                })
                 
-            }
-        });
-        console.log(res.status)
-            if(res.status===200){
-                Swal.fire({
-                    icon:'success',
-                    title:'Booked Successfully'
-                    
-                })
                 setloggedIn(true);
-                const data= await res.json();
+                const data = await res.json();
                 console.log(data);
-                sessionStorage.setItem('parkings', JSON.stringify(data));
+                sessionStorage.setItem('parkings', JSON.stringify(data));   
             }
-        
-            
-            else{ // yaha pe ye condution jab address me kuch glti kr denge tb chlegi, basically jab error occur hoga 
+
+            else { // yaha pe ye condution jab address me kuch glti kr denge tb chlegi, basically jab error occur hoga 
                 Swal.fire({
-                    icon:'error', // error defaullt h yaha
-                    title:'Error',
-                    text:'Something went wrong!!'
+                    icon: 'error', // error defaullt h yaha
+                    title: 'Error',
+                    text: 'Something went wrong!!'
                 })
-            
+
             }
-       
-    }
-});
+
+        },
+        validationSchema: BookSchema
+    });
     return (
         <div className="py-5 vh-100 bg-body-secondary">
             <div className="col-md-4 mx-auto">
@@ -72,22 +106,31 @@ const BookSlot = () => {
                                 <option value={8}> 8</option>
                                 <option value={9}> 9</option>
                                 <option value={10}>10</option>
-                                
+
+
+                            </select>
+
+
+                            <label>Slot</label>
+                            <select className='form-control mb-3' id="slot" onChange={bookslot.handleChange} value={bookslot.values.slot}>
+                                <option value="">Select Slot</option>
+                                {
+                                    slotsAvailable.map(slot => (
+                                        <option value={slot}>{slot}</option>
+                                    ))
+                                }
+
+
 
                             </select>
                             
 
-                            <label>Slot</label>
-
-                            <input id="slot" type="number" className='form-control mb-3' onChange={bookslot.handleChange} value={bookslot.values.slot}/>
-
-
                             <label>Vehicle No. </label>
 
-                            <input id="vehicle" type="text" className='form-control mb-3' onChange={bookslot.handleChange} value={bookslot.values.vehicle}/>
+                            <input id="vehicle" type="text" className='form-control mb-3' onChange={bookslot.handleChange} value={bookslot.values.vehicle} />
 
                             <label>Time </label>
-                            <input id="time" type="datetime-local" className='form-control mb-3' onChange={bookslot.handleChange} value={bookslot.values.time}/>
+                            <input id="time" type="datetime-local" className='form-control mb-3' onChange={bookslot.handleChange} value={bookslot.values.time} />
 
                             <button type="submit" className="btn btn-primary">Submit</button>
                         </form>
