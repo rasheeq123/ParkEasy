d69a1245adfa7bcb852d4463d5f4814df2ced406
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2023-10-28 23:53:01 +0530

    done

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      
-    </div>
+    // <div>
+    //   <div className="container">
+    //   <div className="col-md-4">
+    //     <div className="card">
+    //     <img src="https://cdn.dribbble.com/users/2560254/screenshots/5701893/media/c62a836c1cef8fd2880d78e25927e650.gif" alt="Images" />
+    //     </div>
+    //   </div>
+    //   </div>
+    // </div>
+    <div className="py-5 vh-100 bg-body-secondary">
+            <div className="col-lg-6 mx-auto">
+                <div className="card">
+                        
+                        {/* <img src="https://cdn.dribbble.com/users/2560254/screenshots/5701893/media/c62a836c1cef8fd2880d78e25927e650.gif" alt="Images" /> */}
+                </div>
+            </div>
+        </div>
   )
 }
 
