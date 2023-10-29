0b478df87d11bbf170ae89694dab9e4c8b5445c5
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2023-10-30 02:28:15 +0530

    style

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {Card, CardMedia} from '@mui/material'
 const Home = () => {
   return (
     
@@ -10,14 +10,30 @@ const Home = () => {
     //             </div>
     //         </div>
     //     </div>
-    <div>
-      <div className="container">
+    // <div>
+    //   <div className="container">
         
-      <div className="col-lg-6 mx-auto py-5">
-      <img src="https://cdn.dribbble.com/users/2560254/screenshots/5701893/media/c62a836c1cef8fd2880d78e25927e650.gif" alt="Images" />
-       </div>
-      </div>
-    </div>
+    //   <div className="col-lg-6 mx-auto py-5">
+    //   <img src="https://cdn.dribbble.com/users/2560254/screenshots/5701893/media/c62a836c1cef8fd2880d78e25927e650.gif" alt="Images" />
+    //    </div>
+    //   </div>
+    // </div>
+    <Card>
+      <CardMedia
+      component="img"
+      alt='Sample GIF'
+      height="900"
+      image='https://cdn.dribbble.com/users/2560254/screenshots/5701893/media/c62a836c1cef8fd2880d78e25927e650.gif'
+      style={{ 
+        borderRadius: '125px',
+        padding: '3%', 
+        display: 'flex',
+        justifyContent: 'center',
+        // opacity:'0.8'
+      }}
+      />
+
+    </Card>
   )
 }
 
