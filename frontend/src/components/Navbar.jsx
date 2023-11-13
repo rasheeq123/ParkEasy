b883775273757a0fc6c7cd968c2ccb6980cc99f2
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2023-11-13 23:52:33 +0530

    event

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,85 +3,86 @@ import { NavLink } from 'react-router-dom'
 import useAppContext from '../AppContext';
 
 
-const Navbar = ({mycart}) => {
+const Navbar = ({ mycart }) => {
 
-    const{loggedIn, setloggedIn, logout}= useAppContext();
-    
-   
-    const showLoginOption=()=>{
-        if(loggedIn){
-            return (
-             <li className="nav-item">
-               <button className='btn btn-danger' onClick={logout} >  Logout</button>
+  const { loggedIn, setloggedIn, logout } = useAppContext();
 
-            </li>
-            );
-        }
+
+  const showLoginOption = () => {
+    if (loggedIn) {
+      return (
+        <li className="nav-item">
+          <button className='btn btn-danger' onClick={logout} >  Logout</button>
+
+        </li>
+      );
     }
+  }
 
   return (
     <>
-  <nav className="navbar navbar-expand-lg bg-body-tertiary text-light ">
-    <div className="container-fluid  ">
-      <a className="navbar-brand" href="#">
-        
-      </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon" />
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <NavLink className="nav-link" to = "/">
-                Home
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to = "/login">
-                Login
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to = "/signup">
-                Signup
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to = "/slotlist">
-                Slots
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to = "/bookslot">
-                Book Slot
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to = "/manageslot">
-                Manage Slot
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to = "/about">
-                About
-            </NavLink>
-          </li>
-          {showLoginOption()}
-
-          
-        </ul>
-        
+      <nav className="navbar navbar-expand-custom navbar-mainbg">
+        <a className="navbar-brand navbar-logo" href="#">
+          Navbar
+        </a>
+        <button
+    className="navbar-toggler"
+    type="button"
+    aria-controls="navbarSupportedContent"
+    aria-expanded="false"
+    aria-label="Toggle navigation"
+  >
+          <i className="fas fa-bars text-white" />
+  </button>
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav ml-auto">
+      <div className="hori-selector">
+        <div className="left" />
+        <div className="right" />
       </div>
-    </div>
-  </nav>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/login">
+                Login
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/signup">
+                Signup
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/slotlist">
+                Slots
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/bookslot">
+                Book Slot
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/manageslot">
+                Manage Slot
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/about">
+                About
+              </NavLink>
+            </li>
+            {showLoginOption()}
+
+
+          </ul>
+
+        </div>
+      
+    </nav >
 </>
 
     
