cff7906fdc4eb6585e6d1055b4a9977d5587b834
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2023-11-16 23:17:20 +0530

    app

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -71,6 +71,7 @@ const Navbar = ({ mycart }) => {
               </li>
               <li className="nav-item">
                 <NavLink className="nav-link" to="/about">
+                  
                   About
                 </NavLink>
               </li>
