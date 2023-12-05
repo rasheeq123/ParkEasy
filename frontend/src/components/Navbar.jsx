cec4ea67f222493267b8f0114ce24d2a9fd8f419
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2023-12-05 22:53:46 +0530

    vanilla-tilt

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -23,12 +23,12 @@ const Navbar = ({ mycart }) => {
 
   return (
     <>
-      <nav className="navbar navbar-expand-lg  bg-dark  ">
+      <nav className="navbar navbar-expand-lg  bg-dark  " >
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
 
-            <div class="position-relative py-2 px-4 text-bg-secondary border border-secondary rounded-pill">
-              ParkEasy <i className="bi bi-ev-front-fill"></i> <svg width="1em" height="1em" viewBox="0 0 16 16" class="position-absolute top-100 start-50 translate-middle mt-1" fill="var(--bs-secondary)" xmlns="http://www.w3.org/2000/svg"><path d="M7.247 11.14L2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" /></svg>
+            <div className="position-relative py-2 px-4 text-bg-secondary border border-secondary rounded-pill" data-tilt>
+              ParkEasy <i className="bi bi-ev-front-fill"></i> 
             </div>
           </a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -39,21 +39,21 @@ const Navbar = ({ mycart }) => {
               <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
                 <li className="nav-item active">
                   <NavLink className="nav-link  " to="/">
-                    <div class="position-relative py-2 px-3 text-bg-secondary border border-secondary rounded-pill">
+                    <div className="position-relative py-2 px-3 text-bg-secondary border border-secondary rounded-pill">
                       Home 
                     </div>
                   </NavLink>
                 </li>
                 <li className="nav-item active">
                   <NavLink className="nav-link" to="/login">
-                    <div class="position-relative py-2 px-4 text-bg-secondary border border-secondary rounded-pill">
+                    <div className="  position-relative py-2 px-4 text-bg-secondary border border-secondary rounded-pill">
                       Login 
                     </div>
                   </NavLink>
                 </li>
                 <li className="nav-item">
                   <NavLink className="nav-link" to="/signup">
-                    <div class="position-relative py-2 px-4 text-bg-secondary border border-secondary rounded-pill">
+                    <div className="position-relative py-2 px-4 text-bg-secondary border border-secondary rounded-pill">
                       SignUp 
                     </div>
                   </NavLink>
@@ -61,21 +61,21 @@ const Navbar = ({ mycart }) => {
 
                 <li className="nav-item">
                   <NavLink className="nav-link" to="/slotlist">
-                    <div class="position-relative py-2 px-4 text-bg-secondary border border-secondary rounded-pill">
+                    <div className="position-relative py-2 px-4 text-bg-secondary border border-secondary rounded-pill">
                       Slots 
                     </div>
                   </NavLink>
                 </li>
                 <li className="nav-item">
                   <NavLink className="nav-link" to="/bookslot">
-                    <div class="position-relative py-2 px-4 text-bg-secondary border border-secondary rounded-pill">
+                    <div className="position-relative py-2 px-4 text-bg-secondary border border-secondary rounded-pill">
                       Book SLot 
                     </div>
                   </NavLink>
                 </li>
                 <li className="nav-item">
                   <NavLink className="nav-link" to="/manageslot">
-                    <div class="position-relative py-2 px-4 text-bg-secondary border border-secondary rounded-pill">
+                    <div className="position-relative py-2 px-4 text-bg-secondary border border-secondary rounded-pill">
                       Manage Slot 
                     </div>
                   </NavLink>
@@ -83,7 +83,7 @@ const Navbar = ({ mycart }) => {
                 <li className="nav-item">
                   <NavLink className="nav-link" to="/about">
 
-                    <div class="position-relative py-2 px-4 text-bg-secondary border border-secondary rounded-pill">
+                    <div className="position-relative py-2 px-4 text-bg-secondary border border-secondary rounded-pill">
                       About 
                     </div>
                   </NavLink>
