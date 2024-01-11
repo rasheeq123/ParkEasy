002bebb8b5d5e04de7faaf22644c70379f236580
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2024-01-11 23:31:19 +0530

    grid

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import { Box, Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Container, Grid, Typography } from '@mui/material'
+import { Box, Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Container, Grid, Paper, Typography } from '@mui/material'
 import { Link, NavLink } from 'react-router-dom'
 import video from "../about_video.mp4"
+import styled from '@emotion/styled'
+
+
 
 const Home = () => {
   return (
     <>
-    
     <div
         className="bghome"
         style={{
@@ -16,7 +18,6 @@ const Home = () => {
           alignItems: "center", // Horizontal centering
           backgroundAttachment: "fixed",
           backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.6)), url('/images/A14.png')`,
-          
           backgroundRepeat: "no-repeat",
           backgroundSize: "100%",
           backgroundPosition: 'center',
@@ -41,106 +42,28 @@ const Home = () => {
         <Typography variant="h2" textAlign={"center"} sx={{ mt: 5,mb:10 }}>
           Book your parking slot!
         </Typography>
-        <Container>
-          <Grid
-            container
-            rowSpacing={1}
-            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-          >
-            {/* <Grid xs={6} > */}
-            <Grid item xs={12} sm={6}>
-              <Box
-                sx={{
-                  // mt: 5,
-                  width: "480px",
-                  height: "500px",
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "center",
-                  // alignItems: "center",
-                }}
-              >
-                <Typography variant="h3" fontWeight='semibold' mb={2}>Explore Slots</Typography>
-                <Typography variant="h4" mb={0}>Uncover the possibilities! Navigate through available slots at various positions effortlessly!</Typography>
-                <NavLink to="/user">
-                <Button sx={{textTransform:'none',borderRadius:'28px',mt:4,width:'60%',fontSize:'25px',py:1}} variant='contained' color='primary'>Get a free diagnosis</Button>
-                </NavLink>
-              </Box>
-            </Grid>
-            {/* <Grid xs={6}> */}
-            <Grid item xs={12} sm={6}>
-              <Container
-               sx={{display: "flex",
-               backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.3)), url('/images/G1.jpg')`,
-                  flexDirection: "column",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  borderRadius: 2,
-                  minHeight: 500,
-                  width:'100%',
-                  // m:5,
-                  // mt:10,
-                  // backgroundSize: '100% 100%', // Cover the entire Paper
-                  backgroundSize: "cover",
-                  backgroundPosition: 'center',
-                  }}
-                >
-                </Container>
-            </Grid>
-          </Grid>
-        </Container>
-        <Container sx={{mt:18,}}>
-          <Grid
-            container
-            rowSpacing={1}
-            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-          >
-            
-            {/* <Grid xs={6}> */}
-            <Grid item xs={12} sm={6}>
-              <Container
-               sx={{display: "flex",
-               backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.3)), url('/images/G2.jpg')`,
-                  flexDirection: "column",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  borderRadius: 2,
-                  minHeight: 500,
-                  width:'100%',
-                  // m:5,
-                  // mt:10,
-                  // backgroundSize: '100% 100%', // Cover the entire Paper
-                  backgroundSize: "cover",
-                  backgroundPosition: 'center',
-                  }}
-                >
-                </Container>
-            </Grid>
-            {/* <Grid xs={6}> */}
-            <Grid item xs={12} sm={6}>
-              <Box
-                sx={{
-                  // m:10,
-                  width: "480px",
-                  height: "500px",
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "center",
-                  // alignItems: "center",
-                }}
-              >
-                <Typography variant="h3" mb={2} fontWeight='semibold'>Book Slot</Typography>
-                <Typography mb={0} variant="h4">Your parking, your way! Choose your floor, slot number, input vehicle details, and preferred time – effortlessly secure your parking spot with a seamless booking experience. Your stress-free parking reservation begins here!</Typography>
-                <NavLink to="/user">
-                <Button sx={{textTransform:'none',borderRadius:'28px',mt:4,mb:1,width:'60%',fontSize:'25px',py:1}} variant='contained' color='primary'>Get a free treatment</Button>
-                </NavLink>
-              </Box>
-            </Grid>
-          </Grid>
-        </Container>
+        <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={6} md={8}>
+          <Item>xs=6 md=8</Item>
+        </Grid>
+        <Grid item xs={6} md={4}>
+          <Item>xs=6 md=4</Item>
+        </Grid>
+        <Grid item xs={6} md={4}>
+          <Item>xs=6 md=4</Item>
+        </Grid>
+        <Grid item xs={6} md={8}>
+          <Item>xs=6 md=8</Item>
+        </Grid>
+      </Grid>
+    </Box>
+        
+        
     
     </>
   )
 }
+
 
 export default Home
