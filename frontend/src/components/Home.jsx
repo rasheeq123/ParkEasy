beb6eebffa315c3d86130e15e42df16b050b74c8
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2024-01-11 23:56:39 +0530

    grid set

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -42,22 +42,9 @@ const Home = () => {
         <Typography variant="h2" textAlign={"center"} sx={{ mt: 5,mb:10 }}>
           Book your parking slot!
         </Typography>
-        <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
-        <Grid item xs={6} md={8}>
-          <Item>xs=6 md=8</Item>
-        </Grid>
-        <Grid item xs={6} md={4}>
-          <Item>xs=6 md=4</Item>
-        </Grid>
-        <Grid item xs={6} md={4}>
-          <Item>xs=6 md=4</Item>
-        </Grid>
-        <Grid item xs={6} md={8}>
-          <Item>xs=6 md=8</Item>
-        </Grid>
-      </Grid>
-    </Box>
+        
+        {/* container started  */}
+        
         
         
     
