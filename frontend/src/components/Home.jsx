37bd67e8637e957eff5eeabe7e477a6cef17f422
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2024-01-13 00:05:09 +0530

    set

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -10,14 +10,14 @@ const StyledPaper = styled(Paper)({
   transition: 'transform 0.3s ease-in-out',
   '&:hover': {
     
-      transform: 'scale(0.9)', // Adjust the scale factor as needed
+      transform: 'scale(1.1)', // Adjust the scale factor as needed
     
   },
   '& img': {
     width: '100%',
     height: 'auto',
     display: 'block',
-    transition: 'transform 0.2s ease-in-out', // Added transition for the image
+    transition: 'transform 0.1s ease-in-out', // Added transition for the image
     transform: 'scale(1)', // Set the default scale
   },
   width: '60%', // Adjust the width as needed
@@ -65,16 +65,16 @@ const Home = () => {
         
       {/*--------------- container started ------------------ */}
       <ThemeProvider theme={theme}>
-      <Grid container  alignItems="stretch" justifyContent="center">
+      <Grid container  >
           <Grid item xs={12} sm={4}  md={4} >
           <StyledPaper>
           
           
           <img src="https://cdn.dribbble.com/users/2548965/screenshots/5747061/media/a1f6a7b3d1f799410657409c1a49f270.jpg" alt="Container 2" style={{ width: '100%', marginBottom: 10 }} />
-          <Typography variant="h6">Explore Slots</Typography>
-          <Typography variant="body2">Uncover the possibilities! Navigate through available slots at various positions effortlessly, in a single click!</Typography>
+          <Typography variant="h6">Login</Typography>
+          <Typography variant="body2">Login to our platform effortlessly! Enter your credentials,and enjoy a smooth entry into the world of Parking. Your stress-free parking reservation begins here!</Typography>
           <NavLink to="/slot">
-                <Button sx={{textTransform:'none',borderRadius:'18px',mt:3,mb:1,width:'30%',fontSize:'15px',py:1}} variant='contained' color='secondary'>View Slots</Button>
+                <Button sx={{textTransform:'none',borderRadius:'18px',mt:3,mb:1,width:'40%',fontSize:'15px',py:1}} variant='contained' color='secondary'>Login</Button>
                 </NavLink>
           </StyledPaper>
            </Grid>
@@ -82,28 +82,28 @@ const Home = () => {
 
           {/* second container */}
 
-          <Grid item xs={12} sm={4} md={4}  >
+          <Grid item xs={12} sm={4}  md={4} >
           <StyledPaper>
           
           
-          <img src="https://cdn.dribbble.com/users/2548965/screenshots/5747061/media/a1f6a7b3d1f799410657409c1a49f270.jpg" alt="Container 2" style={{ width: '100%', marginBottom: 10 }} />
-          <Typography variant="h6">Book Slot</Typography>
-          <Typography variant="body2"> Choose your floor, slot number, input vehicle details, preferred time & effortlessly secure your parking spot with a seamless booking experience. </Typography>
-          <NavLink to="/bookslot">
-                <Button sx={{textTransform:'none',borderRadius:'18px',mt:3,mb:1,width:'30%',fontSize:'15px',py:1}} variant='contained' color='secondary'>Book Slot</Button>
+          <img src="https://cdn.dribbble.com/users/6153591/screenshots/14332697/media/be3e2e5ebad63d1c6778dba2f96427f5.jpg" alt="Container 2" style={{ width: '100%', marginBottom: 10 }} />
+          <Typography variant="h6">Explore Slots</Typography>
+          <Typography variant="body2">Uncover the possibilities! Navigate through available slots at various positions effortlessly, in a single click!</Typography>
+          <NavLink to="/slot">
+                <Button sx={{textTransform:'none',borderRadius:'18px',mt:3,mb:1,width:'40%',fontSize:'15px',py:1}} variant='contained' color='secondary'>View Slots</Button>
                 </NavLink>
-        </StyledPaper>
-          </Grid>
+          </StyledPaper>
+           </Grid>
 
           {/* --------------third container */}
 
           <Grid item xs={12} sm={4}  md={4}  >
           <StyledPaper>
-          <img src="https://cdn.dribbble.com/users/2548965/screenshots/5747061/media/a1f6a7b3d1f799410657409c1a49f270.jpg" alt="Container 2" style={{ width: '100%', marginBottom: 10 }} />
+          <img src="https://cdn.dribbble.com/users/32512/screenshots/4341070/media/6d8c45ae66945f1febf2703d1b628ccb.gif" alt="Container 2" style={{ height: '100%', width: '100%', marginBottom: 10 }} />
           <Typography variant="h6">Book Slot</Typography>
           <Typography variant="body2"> Choose your floor, slot number, input vehicle details, preferred time & effortlessly secure your parking spot with a seamless booking experience. </Typography>
           <NavLink to="/bookslot">
-                <Button sx={{textTransform:'none',borderRadius:'18px',mt:3,mb:1,width:'30%',fontSize:'15px',py:1}} variant='contained' color='secondary'>Book Slot</Button>
+                <Button sx={{textTransform:'none',borderRadius:'18px',mt:3,mb:1,width:'40%',fontSize:'15px',py:1}} variant='contained' color='secondary'>Book Slot</Button>
                 </NavLink>
         </StyledPaper>
           </Grid>
